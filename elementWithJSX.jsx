af699375dcc023d531d0b444c5799afac17fc12a
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-17 21:13:21 +0300

    Create elementWithJSX.jsx

---
+++ elementWithJSX.jsx
@@ -0,0 +1,6 @@
+/* This code renders <p> element using JSX */
+
+const paragraph = <p>This is a paragraph using JSX!</p>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(paragraph);
