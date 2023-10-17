8833b08a761327b0ff8751384dc07e76df1c2768
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-17 21:06:08 +0300

    Create vehicle.jsx

---
+++ vehicle.jsx
@@ -0,0 +1,5 @@
+/* this code extracts the third item from the vehicles array and assigns it to the variable suv. */
+
+const vehicles = ['mustang', 'f-150', 'expedition'];
+
+const [,,suv] = vehicles;
