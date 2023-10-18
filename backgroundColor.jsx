f74324972656ae2c9bdd7a986b2cfeaccb3f6538
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-18 21:04:44 +0300

    Create backgroundColor.jsx

---
+++ backgroundColor.jsx
@@ -0,0 +1,10 @@
+/* This code sets the style - background-color of a h1 to yellow */
+
+const Header = () => {
+  return (
+    <>
+      <h1 style={{backgroundColor: "yellow"}}>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
+}
