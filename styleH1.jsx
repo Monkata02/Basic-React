bc3a3c8a080ccf3f658bae51f61e7c25fe5317c3
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-18 21:02:53 +0300

    Create styleH1.jsx

---
+++ styleH1.jsx
@@ -0,0 +1,11 @@
+/* This code sets a style to the h1 {{color: "purple"}} */
+
+
+const Header = () => {
+  return (
+    <>
+      <h1 style={{color: "purple"}}>Hello Style!</h1>
+      <p>Add a little style!</p>
+    </>
+  );
+}
