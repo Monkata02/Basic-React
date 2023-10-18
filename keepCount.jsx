d1ff439482116c72ef5a56389682ae516f819505
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-18 20:55:42 +0300

    Create keepCount.jsx

---
+++ keepCount.jsx
@@ -0,0 +1,8 @@
+/* This code is a React functional component named KeepCount that uses the useState hook to manage and display a count. */
+
+
+import { useState } from "react";
+
+function KeepCount() {
+  const [count, setCount] = useState(0);
+}
