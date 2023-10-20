22ee28d01204392f86d1b19c8f5a2a4ea8453ef0
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-20 19:19:56 +0300

    Create liArr.jsx

---
+++ liArr.jsx
@@ -0,0 +1,11 @@
+/* transform an array of products into an array of <li> items */
+
+const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);
+
+return (
+  <ul>{listItems}</ul>
+);
