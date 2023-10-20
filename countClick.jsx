177d69a729a406c54be49a3ca447632373a316e7
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-20 19:22:14 +0300

    Create countClick.jsx

---
+++ countClick.jsx
@@ -0,0 +1,16 @@
+/* Clicking this button will increment the counter: */
+
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
