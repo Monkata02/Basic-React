7da75eb01faf44e37516c0d5ece61ce702c1236c
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-20 19:21:15 +0300

    Create onClickEvent.jsx

---
+++ onClickEvent.jsx
@@ -0,0 +1,14 @@
+/* Responding to events */
+
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
